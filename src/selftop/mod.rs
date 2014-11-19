25384d1398bd576eed4365d03597b84d268b5126
Alexey Kolmakov <alxkolm@bk.ru>
2014-11-19 21:15:55 +0300

    clear code

--- src/selftop/mod.rs
+++ src/selftop/mod.rs
@@ -7,14 +7,6 @@ pub struct MotionSniffer {
 }
 
 impl MotionSniffer {
-	// pub fn new () -> MotionSniffer<'a>{
-	// 	// let time: Time = 0;
-	// 	// let mcount: u64 =0;
-	// 	MotionSniffer{
-	// 		last_event_time: &mut 0,
-	// 		motion_count: &mut 0
-	// 	}
-	// }
 	pub fn processEvent(&mut self, time: Time){
 	
 		let delta = time - self.last_event_time;
@@ -22,7 +14,7 @@ impl MotionSniffer {
 		if delta > 200 || self.last_event_time == 0 {
 			self.motion_count += 1;
 		}
-		
+
 		self.last_event_time = time;
 	}
 }
